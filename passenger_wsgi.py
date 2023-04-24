9972fc5ce3a010487fbaea7049ed5ac098b6d37a
root <root@142-4-9-202.webhostbox.net>
2023-04-24 22:28:06 +0200

    cleanup

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from shootbe.wsgi import application
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shootbe.settings")
