43bed93fd179ac2d29435725dcc2d020d99075b4
Adam Dekan <adam.dekan@gmail.com>
2023-05-03 00:09:09 +0200

    socket 142-...

--- shootbe/settings.py
+++ shootbe/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 
-if socket.gethostname() == "lensuno.com":
+if socket.gethostname() == "142-4-9-202.webhostbox.net":
     DEBUG = False
     ALLOWED_HOSTS = [
         "www.lensuno.com",
